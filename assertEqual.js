fffeff8c37dfd531f0d5c9befbe52dd0d4fefb61
Lauren Johnston <hilotraining@outlook.com>
2023-01-09 18:43:14 -0700

    Made a funtion to practice console.assert and added emojis

---
+++ assertEqual.js
@@ -0,0 +1,10 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(String.fromCodePoint(0x1F7E2) + String.fromCodePoint(0x1F7E2) + String.fromCodePoint(0x1F7E2) + ' Assertion Passed: ' + actual + ' === ' + expected);
+  }
+  return console.log(String.fromCodePoint(0x1F534) + String.fromCodePoint(0x1F534) + String.fromCodePoint(0x1F534) + ' Assertion Failed: ' + actual + ' !== ' + expected);
+};
+
+// TEST CODE
+assertEqual("Bootcamp", "Bootcamp"); //error
+assertEqual(2, 1); //no message
