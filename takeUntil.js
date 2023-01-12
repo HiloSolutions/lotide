0b1f053a8ffa7ab82351b748d00856b92c75c4ce
Lauren Johnston <hilotraining@outlook.com>
2023-01-12 16:56:38 -0700

    takeUntil added

---
+++ takeUntil.js
@@ -0,0 +1,25 @@
+//take in array and callback
+const takeUntil = (array, callback) => {
+  const result = [];
+
+  for (const item of array) {
+    if (callback(item)) {
+      return result;
+    }
+    result.push(item);
+  }
+
+  return result;
+};
+
+const test = x => x < 0;
+//INVOKE
+const data1 = [1, -2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, test);
+console.log(results1); //[ 1, 2, 5, 7, 2 ]
+
+console.log('---');
+
+const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
+const results2 = takeUntil(data2, x => x === ',');
+console.log(results2);//[ 'I\'ve', 'been', 'to', 'Hollywood' ]
