b9018224d711be90b0d8b8a7e677e50d7a095122
Lauren Johnston <hilotraining@outlook.com>
2023-01-16 09:40:57 -0700

    refactored assertArraysEqual to include test file

--- assertArraysEqual.js
+++ assertArraysEqual.js
@@ -1,19 +1,3 @@
-//Takes in two arrays to test equality
-const eqArrays = function(arr1, arr2) {
-  
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-
-  return true;
-};
-
 //test return of equality with the assert Equal argument
 const assertArraysEqual = function(eqArrays, expected) {
   if (eqArrays === expected) {
@@ -22,10 +6,5 @@ const assertArraysEqual = function(eqArrays, expected) {
   return console.log(`${String.fromCodePoint(0x1F534)} Assertion Failed: ${eqArrays} !== ${expected}`);
 };
 
-//TEST CODE
-
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => pass
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), false); // => fail
-assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => pass
-assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => fail
-assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+//EXPORT
+module.exports = assertArraysEqual;
---
+++ test/assertArraysEqualTest.js
@@ -0,0 +1,15 @@
+//IMPORT
+const assertArraysEqual = require('../assertArraysEqual.js');
+const eqArrays = require('../eqArrays.js');
+
+//TEST CODE
+
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => pass
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), false); // => fail
+assertArraysEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => pass
+assertArraysEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), true); // => fail
+assertArraysEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+//EXPORT
+module.exports = assertArraysEqual;
+module.exports = eqArrays;
